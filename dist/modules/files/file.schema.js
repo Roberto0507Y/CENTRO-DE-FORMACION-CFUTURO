18a699e8ce83df600eeab19a55886f5df76eb6b8
Roberto0507Y <jonatanruiz918@gmail.com>
2026-04-14 22:52:29 -0600

    Deja de versionar builds dist

--- dist/modules/files/file.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileIdParamsSchema = void 0;
-const zod_1 = require("zod");
-exports.fileIdParamsSchema = zod_1.z
-    .object({
-    id: zod_1.z.preprocess((v) => Number(v), zod_1.z.number().int().positive()),
-})
-    .strict();
--- dist/modules/gradebook/gradebook.schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.courseIdParamsSchema = void 0;
-const zod_1 = require("zod");
-const idFromParam = zod_1.z.preprocess((v) => Number(v), zod_1.z.number().int().positive());
-exports.courseIdParamsSchema = zod_1.z.object({ courseId: idFromParam }).strict();
